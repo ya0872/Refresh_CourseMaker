c2fbd8bdff1a6ca4f193798fd7a0339f8afd5134
ya0872 <c1460807@st.kanazawa-it.ac.jp>
2025-10-31 14:35:13 +0900

    ルーティング機能の追加

---
+++ vite-project/src/Login.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react'
+
+export const Login = () => {
+    return(
+        <div className='Login'>
+            <h2>Login Page</h2>
+            <nav>
+                <Link to="/">Go to HomePage</Link>
+            </nav>
+        </div>
+    )
+}
+
+export default Login;
